6de17519a41736a32da8b27679e53fd105623e1b
Andrew Nasibullin <duhich02@yandex.ru>
2023-05-07 15:16:24 +0300

    Добавил кнопки выбора курсор/смещение/вращение/маштабирование в компоненте Button с помощью q-radio

--- quasar-project/src/components/BabylonScene.vue
+++ quasar-project/src/components/BabylonScene.vue
@@ -1,10 +1,12 @@
 <template>
   <canvas width="1480" height="750" />
+  <Button style="position: absolute; top: 0; left: 0;"/>
 </template>
 
 <script setup>
 import { onMounted } from "@vue/runtime-core";
 import { BasicScene } from 'src/scenes/MyFirstScene.ts';
+import Button from "src/components/Button.vue"
 
 onMounted(() => {
   const canvas = document.querySelector("canvas");
---
+++ quasar-project/src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm" style="color: white">
+      <q-radio v-model="radio" val="action" label="Курсор" />
+      <q-radio v-model="radio" val="position" label="Смещение" />
+      <q-radio v-model="radio" val="rotation" label="Вращение" />
+      <q-radio v-model="radio" val="scaling" label="Масштабирование" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const radio = ref<string>("rotation");
+
+</script>
+
+<style lang="sass" scoped>
+</style>
