911b231ac699bf9e905ceb5a45aa0fa5f0503cf6
Andrew Nasibullin <duhich02@yandex.ru>
2023-05-07 19:27:55 +0300

    Реализовал перемещение и маштабирование объектов

--- quasar-project/src/scenes/MyFirstScene.ts
+++ quasar-project/src/scenes/MyFirstScene.ts
@@ -10,13 +10,13 @@ import {
   Nullable,
   AbstractMesh,
   UtilityLayerRenderer,
-
 } from "@babylonjs/core";
 import {
   GizmoManager,
   RotationGizmo,
+  ScaleGizmo,
+  PositionGizmo,
 } from "@babylonjs/core/Gizmos";
-
 
 export class BasicScene {
   scene: Scene;
@@ -24,11 +24,9 @@ export class BasicScene {
   selectionMaterial: StandardMaterial;
   unselectedMaterial: StandardMaterial;
   selectedMesh: Nullable<AbstractMesh> = null;
-
   constructor(private canvas: HTMLCanvasElement) {
     this.engine = new Engine(this.canvas, true);
     this.scene = this.CreateScene();
-
     this.selectionMaterial = new StandardMaterial(
       "selection-material",
       this.scene
@@ -39,23 +37,18 @@ export class BasicScene {
       this.scene
     );
     this.unselectedMaterial.diffuseColor = Color3.Purple();
-
     this.engine.runRenderLoop(() => {
       this.scene.render();
     });
   }
-
   CreateScene(): Scene {
     const scene = new Scene(this.engine);
-
     const camera = new FreeCamera("camera1", new Vector3(5, 5, -5), this.scene);
     camera.attachControl(this.canvas, true);
-
     const light = new HemisphericLight("light", new Vector3(0.0, 0.0, 1.0), this.scene);
     light.intensity = 0.5;
-
     const ground = MeshBuilder.CreateGround("ground", { width: 6, height: 4 }, this.scene);
-
+    ground.position = new Vector3(0, -1, 0);
     const box = MeshBuilder.CreateBox("box", { size: 4 }, scene);
     box.position.z = 9;
     const box2 = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
@@ -66,27 +59,26 @@ export class BasicScene {
     box.material = material;
     box2.material = material;
     ground.material = secondMaterial
-
     // create GizmoManager instance and set usePointerToAttachGizmos to true
     const gizmoManager = new GizmoManager(scene);
     gizmoManager.usePointerToAttachGizmos = true;
-
     scene.onPointerDown = (evt, pickResult) => {
       if (pickResult?.hit && pickResult.pickedMesh instanceof AbstractMesh) {
         this.selectMesh(pickResult.pickedMesh);
-
         // create RotationGizmo instance and attach it to the selected mesh
-        const utilityLayer = new UtilityLayerRenderer(scene);
-        const rotationGizmo = new RotationGizmo(utilityLayer);
+        const utilityLayerRotate = new UtilityLayerRenderer(scene);
+        const rotationGizmo = new RotationGizmo(utilityLayerRotate);
         rotationGizmo.attachedMesh = this.selectedMesh;
+        // create ScalingGizmo instance and attach it to the selected mesh
+        const utilityLayerScale = new UtilityLayerRenderer(scene);
+        const scalingGizmo = new ScaleGizmo(utilityLayerScale);
+        scalingGizmo.attachedMesh = this.selectedMesh;
       } else {
         this.unselectMesh();
       }
     };
-
     return scene;
   }
-
   selectMesh(mesh: AbstractMesh) {
     if (mesh.material !== this.selectionMaterial) {
       if (this.selectedMesh && this.selectedMesh !== mesh) {
@@ -94,8 +86,19 @@ export class BasicScene {
       }
       this.selectedMesh = mesh;
       mesh.material = this.selectionMaterial;
+
+      // create ScaleGizmo instance and attach it to the selected mesh
+      const utilityLayer = new UtilityLayerRenderer(this.scene);
+      const scaleGizmo = new ScaleGizmo(utilityLayer);
+      scaleGizmo.attachedMesh = mesh;
+
+      // create TranslationGizmo instance and attach it to the selected mesh
+      const utilityLayerTranslate = new UtilityLayerRenderer(this.scene);
+      const translationGizmo = new PositionGizmo(utilityLayerTranslate);
+      translationGizmo.attachedMesh = mesh;
     }
   }
+
 
   unselectMesh() {
     if (this.selectedMesh) {
