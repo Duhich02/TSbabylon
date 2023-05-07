0a319c659dd7e53824bf9a440b363e3108ba0dc1
Andrew Nasibullin <duhich02@yandex.ru>
2023-05-07 15:49:54 +0300

    Реализовал выбор объектов путем изменения цвета выбранной фигуры

--- quasar-project/src/scenes/MyFirstScene.ts
+++ quasar-project/src/scenes/MyFirstScene.ts
@@ -1,44 +1,89 @@
-import { Engine, Scene, FreeCamera, Vector3, MeshBuilder, StandardMaterial, Color3, HemisphericLight } from "@babylonjs/core";
-// const createScene = (canvas) => {
-export class BasicScene{
+import {
+  Engine,
+  Scene,
+  FreeCamera,
+  Vector3,
+  MeshBuilder,
+  StandardMaterial,
+  Color3,
+  HemisphericLight,
+  Nullable,
+  AbstractMesh,
+} from "@babylonjs/core";
+
+export class BasicScene {
   scene: Scene;
   engine: Engine;
-  constructor(private canvas:HTMLCanvasElement){
-    this.engine = new Engine(this.canvas, true)
-    this.scene = this.CreateScene()
+  selectionMaterial: StandardMaterial;
+  unselectedMaterial: StandardMaterial;
+  selectedMesh: Nullable<AbstractMesh> = null;
 
+  constructor(private canvas: HTMLCanvasElement) {
+    this.engine = new Engine(this.canvas, true);
+    this.scene = this.CreateScene();
+
+    this.selectionMaterial = new StandardMaterial(
+      "selection-material",
+      this.scene
+    );
+    this.selectionMaterial.diffuseColor = Color3.Green();
+    this.unselectedMaterial = new StandardMaterial(
+      "unselected-material",
+      this.scene
+    );
+    this.unselectedMaterial.diffuseColor = Color3.Purple();
 
     this.engine.runRenderLoop(() => {
       this.scene.render();
     });
-
   }
-  CreateScene():Scene{
+
+  CreateScene(): Scene {
     const scene = new Scene(this.engine);
 
     const camera = new FreeCamera("camera1", new Vector3(5, 5, -5), this.scene);
-    // camera.setTarget(Vector3.Zero());
-    camera.attachControl(this.canvas, true); //mouse
+    camera.attachControl(this.canvas, true);
 
     const light = new HemisphericLight("light", new Vector3(0.0, 0.0, 1.0), this.scene);
     light.intensity = 0.5;
 
-    const ground = MeshBuilder.CreateGround("ground", { width: 4, height: 4 }, this.scene);
+    const ground = MeshBuilder.CreateGround("ground", { width: 6, height: 4 }, this.scene);
 
     const box = MeshBuilder.CreateBox("box", { size: 4 }, scene);
     box.position.z = 9;
     const box2 = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
     const material = new StandardMaterial("box-material", scene);
-    const groundMaterial = new StandardMaterial("box-material", scene);
+    const secondMaterial = new StandardMaterial("box-material", scene);
     material.diffuseColor = Color3.Purple();
-    groundMaterial.diffuseColor = Color3.Black();
-    ground.material = groundMaterial
+    secondMaterial.diffuseColor = Color3.Black();
     box.material = material;
     box2.material = material;
+    ground.material = secondMaterial
 
-    this.engine.runRenderLoop(() => {
-      scene.render();
-    });
+    scene.onPointerDown = (evt, pickResult) => {
+      if (pickResult?.hit && pickResult.pickedMesh instanceof AbstractMesh) {
+        this.selectMesh(pickResult.pickedMesh);
+      } else {
+        this.unselectMesh();
+      }
+    };
     return scene;
+  }
+
+  selectMesh(mesh: AbstractMesh) {
+    if (mesh.material !== this.selectionMaterial) {
+      if (this.selectedMesh && this.selectedMesh !== mesh) {
+        this.selectedMesh.material = this.unselectedMaterial;
+      }
+      this.selectedMesh = mesh;
+      mesh.material = this.selectionMaterial;
+    }
+  }
+
+  unselectMesh() {
+    if (this.selectedMesh) {
+      this.selectedMesh.material = this.unselectedMaterial;
+      this.selectedMesh = null;
+    }
   }
 }
